c69cdc799b0cd6fca42d4c6e3e18dfa4e013206f
JakubSzczerba <jakszczerb@gmail.com>
2022-11-20 17:41:55 +0100

    Create panel for meters.

--- src/Entity/Meter.php
+++ src/Entity/Meter.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use App\Repository\MeterRepository;
